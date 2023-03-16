02ce9ae3d95a0e7ec3d8dfe002a5b0e91f15e887
dior <dior47m@gmail.com>
2023-03-16 23:34:23 +0500

    fixed many bugs, updated layout

--- js/profile.js
+++ js/profile.js
@@ -1,13 +1,47 @@
-const auc_category_slider = new Swiper('.mobile_winned_aucksions', {
-    direction: 'horizontal',
-    slidesPerView: '2.5',
+const auc_category_slider = new Swiper(".mobile_winned_aucksions", {
+  direction: "horizontal",
+  slidesPerView: "2.5",
+  speed: 500,
+  spaceBetween: 15,
+  observer: true,
+  observeParents: true,
+  pagination: {
+    el: ".mobile_winned_aucksions_pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
+
+const row2__col2__checklist_carousel = new Swiper(
+  ".row2__col2__checklist_carousel",
+  {
+    direction: "horizontal",
+    slidesPerView: 2,
     speed: 500,
     spaceBetween: 15,
     observer: true,
     observeParents: true,
     pagination: {
-        el: '.mobile_winned_aucksions_pagination',
-        type: 'bullets',
-        clickable: true,
+      el: ".checklist_carousel_pagination",
+      type: "bullets",
+      clickable: true,
     },
-});
+  }
+);
+
+const row2__col2__checklist_carousel2 = new Swiper(
+  ".row2__col2__checklist_carousel2",
+  {
+    direction: "horizontal",
+    slidesPerView: 1,
+    speed: 500,
+    spaceBetween: 15,
+    observer: true,
+    observeParents: true,
+    pagination: {
+      el: ".checklist_carousel_pagination2",
+      type: "bullets",
+      clickable: true,
+    },
+  }
+);
